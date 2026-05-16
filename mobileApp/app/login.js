f9bd919297f4fb6b3193d9e2097ae4338f580171
Ayush-T-iwar-i <11tiwari11ayush@gmail.com>
2026-05-16 10:54:54 +0530

    Fix Image import

--- mobileApp/app/login.js
+++ mobileApp/app/login.js
@@ -2,7 +2,7 @@
 import {
   View, Text, StyleSheet, TextInput, Pressable,
   ActivityIndicator, StatusBar, KeyboardAvoidingView,
-  Platform, ScrollView, Alert,
+  Platform, ScrollView, Alert,Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
